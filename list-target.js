b26269d8c6b5c75038f548f93193cd08141af4e3
System233 <cdkeylzh@outlook.com>
2024-09-14 13:58:49 +0800

    测试构建

--- list-target.js
+++ list-target.js
@@ -1,5 +1,11 @@
 #!/bin/env node
-import {readFile} from 'fs/promises'
-const data=await readFile('./list.csv','utf8')
-const result=data.split('\n').flatMap(x=>x.split(',',1)).map(x=>x.trim()).filter(x=>!!x.length)
-console.log(JSON.stringify(result))
+import { readFile } from "fs/promises";
+const listFile = process.env.LIST_FILE ?? "./list.csv";
+const data = await readFile(listFile, "utf8");
+const result = data
+  .split("\n")
+  .filter((x) => !x.startsWith("#"))
+  .flatMap((x) => x.split(",", 1))
+  .map((x) => x.trim())
+  .filter((x) => !!x.length);
+console.log(JSON.stringify(result));
