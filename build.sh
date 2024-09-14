2780e7c9da9fcce127b07e943e8a8ad27f00c897
System233 <cdkeylzh@outlook.com>
2024-09-14 18:26:55 +0800

    测试构建

--- build.sh
+++ build.sh
@@ -4,15 +4,23 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-export PATH=$PATH:$(pwd)/node_modules/.bin
+CWD=$(pwd)
+export PATH=$PATH:${CWD}/node_modules/.bin
 
+cd "${REPO_ROOT}"
 if [ ! -e "$DIR" ]; then
-    yarn ll-helper convert "$APP_ID" --name "$APP_NAME" --with-linyaps --from ./template --quiet --cache-dir ${CACHE_DIR}
-    cp -v ./.gitignore "${NEW_APP_ID}"
+    yarn ll-helper convert "$APP_ID" --name "$APP_NAME" --with-linyaps --from "${CWD}/com.uniontech.foundation" --quiet --cache-dir "${CACHE_DIR}"
+    cp -v ./.gitignore "${APP_DIR}"
 fi
-cd "${NEW_APP_ID}"
+cd "${APP_DIR}"
 echo CWD=$(pwd)
+
 ll-helper patch ld icon
 
-ls -l
+echo [搜索依赖]
 ll-helper resolve --cache-dir ${CACHE_DIR}
+
+echo [正式构建]
+ll-builder build
+echo [导出Layer]
+ll-builder export -l
