4afe43400744a8d35690d160a4dfc0cfe645fae1
System233 <cdkeylzh@outlook.com>
2024-09-14 14:49:29 +0800

    fix: 添加.bin到PATH fix: 安装玲珑

--- build.sh
+++ build.sh
@@ -4,10 +4,14 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+export PATH=$PATH:$(pwd)/node_modules/.bin
+
 if [ ! -e "$DIR" ]; then
     yarn ll-helper convert "$APP_ID" --name "$APP_NAME" --with-linyaps --from ./template
     cp -v ./.gitignore "${NEW_APP_ID}"
 fi
 cd "${NEW_APP_ID}"
+echo CWD=$(pwd)
+ls -l
 yarn ll-helper patch ld icon
 yarn ll-helper resolve
