8a252f2d34637c2925aa4f949481dfbc7c39e41b
System233 <cdkeylzh@outlook.com>
2024-09-15 00:58:31 +0800

    修复runtime，重构部分包

---
+++ fix_duplicate_apt_entries.sh
@@ -0,0 +1,9 @@
+# Copyright (c) 2024 System233
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+while read line; do
+    echo $line
+    cat $line | sort -u >$line
+done <<<$(find repos -name "sources.list")
