79cd0a4952a50ddb23aa009ced3287661fea753f
System233 <cdkeylzh@outlook.com>
2024-09-16 16:07:02 +0800

    修复缓存

--- build_cache.sh
+++ build_cache.sh
@@ -2,20 +2,24 @@
 
 set -e
 
+export PATH=$PATH:$(pwd)/node_modules/.bin
+
 echo [准备缓存]
 
 DIR=$(dirname $0)
-function build(){
+function build() {
     cd "$DIR/$1"
-    apt-cli resolve "example" -c "${CACHE_DIR}" -f "./sources.list" 
-    apt-cli find "example" -c "${CACHE_DIR}" -f "./sources.list" 
+    apt-cli resolve "example" -c "${CACHE_DIR}" -f "./sources.list"
+    apt-cli find "example" -c "${CACHE_DIR}" -f "./sources.list"
     ll-builder build
     ll-builder run
 }
 
 build com.uniontech.foundation &
+pid1=$!
 build org.deepin.foundation &
+pid2=$!
 
-wait
+wait $pid1 && wait $pid2
 
 echo 缓存已就绪
